01a45b72239712cebda3f8daaf8639ffae88f2d6
Beau Charman <beau.charman@gmail.com>
2014-02-14 08:25:03 +1100

    Create cache.php

---
+++ samurai/library/extensions/cache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Cahce
+ * ======================================================
+ * cache.php
+ * @version      14th | February 9th 2013
+ * @package      WordPress
+ * @subpackage   samurai
+ *
+ * http://codex.wordpress.org/Class_Reference/WP_Object_Cache
+ */
+
+
+
+class Samurai_Cache
+{
+
+ // coming soon...
+
+}
