a3a0cc812f1b6a01e0f0b0011421786f5feea4bf
Beau Charman <beau.charman@gmail.com>
2014-04-22 22:01:56 +1000

    Update assets.php

--- samurai/library/extensions/assets.php
+++ samurai/library/extensions/assets.php
@@ -19,14 +19,24 @@ class Samurai_Asset
 	{
 
 		/**
-		 * Register and Enqeue local JavaScripts
+		 * Register and enqeue frontend assets
 		 */
-		add_action('wp_enqueue_scripts', array(&$this, 'load_javascripts'));
+		if (! is_admin() && ! in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php')))
+		{
+			/* Frontend JavaScripts */
+			add_action('wp_enqueue_scripts', array(&$this, 'load_frontend_javascripts'));
+
+			/* Frontend Stylesheets */
+			add_action('init', array(&$this, 'load_frontend_stylesheets'));
+		}
 
 		/**
-		 * Register and enqeue stylesheets action
+		 * Register and enqeue admin stylesheets 
 		 */
-		add_action('init', array(&$this, 'load_stylesheets'));
+		if(is_admin()) 
+		{
+			add_action('init', array(&$this, 'load_admin_stylesheets'));	
+		}
 
 		/**
 		 * Custom Editor Styles action
@@ -55,10 +65,10 @@ class Samurai_Asset
 
 	/**
  	 *
- 	 * Load JavaScripts
+ 	 * Load front JavaScripts
 	 *
 	 */
-	static function load_javascripts()
+	static function load_frontend_javascripts()
 	{
 		/**
 		 * Register scripts here
@@ -72,112 +82,91 @@ class Samurai_Asset
 
 
 		/**
-	 	 * Enqueue frontend scripts here
+		 * Dequeue the currently registered version of jQuery
 		 */
-		if (! is_admin())
+		wp_dequeue_script('jquery');
+
+
+
+		/**
+		 * Enqueue JavaScripts
+		 */
+		
+		/* Comments */
+		if (is_singular() && get_option('thread_comments') && SAMURAI_ENABLE_COMMENTS)
 		{
-			/**
-			 * Dequeue the currently registered version of jQuery
-			 */
-			wp_dequeue_script('jquery');
-
-			/* Comments */
-			if (is_singular() && get_option('thread_comments') && SAMURAI_ENABLE_COMMENTS)
-			{
-				wp_enqueue_script('comment-reply');
-			}
-
-			/**
-			 * Modernizr
-			 */
-			// wp_enqueue_script('samurai_modernizr');
-
-			/**
-			 * Load in separate scripts for development, change this to a concatenated
-			 * file for deployment. See library/extentions/config.php
-			 */
-			if (SAMURAI_DEVELOPMENT_MODE)
-			{
-				/**
-				 * jQuery
-				 */
-				// wp_enqueue_script('samurai_jquery'); // theme version
-				// wp_enqueue_script('jquery'); // wp-includes version
-				// wp_enqueue_script('google_jquery'); // Google hosted version
-
-				/**
-				 * Plugins
-				 */
-				wp_enqueue_script('samurai_plugins');
-
-				/**
-				 * Enqueue other theme template scripts for developement,
-				 * or contitional production scripts here.
-				 */
-			}
-
-			/**
-			 * Main project JavaScript
-			 */
-			wp_enqueue_script('samurai_main');
+			wp_enqueue_script('comment-reply');
 		}
+
+		/* Modernizr */
+		// wp_enqueue_script('samurai_modernizr');
+
+		// Load in separate scripts for development, change this to a concatenated
+		// file for deployment. See library/extentions/config.php
+		
+		if (SAMURAI_DEVELOPMENT_MODE)
+		{
+			/* jQuery */
+			// wp_enqueue_script('samurai_jquery'); // theme version
+			// wp_enqueue_script('jquery'); // wp-includes version
+			// wp_enqueue_script('google_jquery'); // Google hosted version
+
+			/* Plugins */
+			wp_enqueue_script('samurai_plugins');
+
+			// Enqueue other theme template scripts for developement,
+			// or conditional production scripts here.
+		}
+
+		/* Main project JavaScripts */
+		wp_enqueue_script('samurai_main');
 	}
 
 
 
 	/**
 	 *
-	 * Load Stylesheets
+	 * Load frontend stylesheets
 	 *
 	 */
-	static function load_stylesheets()
+	static function load_frontend_stylesheets()
 	{
 		/**
-		 * Register styles here
+		 * Register styles
 		 */
+		
+		/* Main stylesheet */
 		wp_register_style('samurai_main_stylesheet', SAMURAI_FULL_STYLES_PATH . '/main.css', array(), SAMURAI_STYLES_CACHE_BREAK);
 
 
 
 		/**
-		 * Enqueue styles here
+		 * Enqueue styles
 		 */
-		if (! is_admin())
-		{
-			/**
-			 *
-			 * Front end stylesheets
-			 *
-			 */
 
-			/**
-			 * Main stylesheet
-			 */
-			wp_enqueue_style('samurai_main_stylesheet');
+		/* Main stylesheet */
+		wp_enqueue_style('samurai_main_stylesheet');
 
-			/**
-			 * Enqueue theme styles here.
-			 * Consider seperate files for development, then bundle into style.css
-			 * for deployment. Conditional styles would be appropriate to be loaded here.
-			 */
-		}
-		elseif (is_admin())
-		{
-			/**
-			 *
-			 * Admin stylesheets
-			 *
-			 */
+		// Enqueue theme styles here.
+		// Consider seperate files for development, then bundle into style.css for production. 
+		// Conditional and contextual styles would be appropriate to be loaded here.
+	}
 
-			// Enqueue admin styles here.
-		}
+	/**
+	 *
+	 * Load admin stylesheets
+	 *
+	 */
+	static function load_admin_stylesheets()
+	{
+		// Register and enqueue admin styles here.
 	}
 
 
 
 	/**
 	 *
-	 * Custom Login Styles
+	 * Custom login styles
 	 *
 	 */
 	static function custom_login_styles()
